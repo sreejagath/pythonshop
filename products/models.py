7c72109a276ee24da0a2a0df4a476b07fd7b675b
Jagal Sree <jagalsree007@gmail.com>
2021-03-15 15:46:45 +0530

    admin panel

--- products/models.py
+++ products/models.py
@@ -6,3 +6,7 @@ class Product(models.Model):
     price=models.FloatField()
     stock=models.IntegerField()
     image=models.CharField(max_length=3500)
+class offers(models.Model):
+    code=models.CharField(max_length=20)
+    description=models.CharField(max_length=500)
+    discount=models.FloatField()
