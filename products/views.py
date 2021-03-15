c598806582fca9eb616ba3bf7db5c1221f45ee43
Jagal Sree <jagalsree007@gmail.com>
2021-03-15 17:37:14 +0530

    data entry

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Product
 # Create your views here.
 def index(request):
-    products=Product.objects.all()
-    return render(request,'index.html')
+    jagal=Product.objects.all()
+    return render(request,'index.html',{'jagal':jagal})
 def about(request):
     return HttpResponse("<h2>This is Jagal Ser</h2>")
