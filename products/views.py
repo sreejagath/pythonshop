a9c29e3ca077c6d5217ddad51424e2184954a84e
Jagal Sree <jagalsree007@gmail.com>
2021-03-15 19:37:22 +0530

    front end

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,offers
 # Create your views here.
 def index(request):
     jagal=Product.objects.all()
-    return render(request,'index.html',{'jagal':jagal})
+    arjun=offers.objects.all()
+    return render(request,'index.html',{'jagal':jagal,'arjun':arjun})
 def about(request):
     return HttpResponse("<h2>This is Jagal Ser</h2>")
